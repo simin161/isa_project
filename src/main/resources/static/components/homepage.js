d332c70365ef24501c252a14ab422043dd9f386a
simin161 <simin.ra161.2018@uns.ac.rs>
2021-11-14 12:58:58 +0100

    fix: Add vue.js.

---
+++ src/main/resources/static/components/homepage.js
@@ -0,0 +1,64 @@
+Vue.component('homepage', {
+	data: function(){
+		return{	}
+	},
+template: `	
+			<div>
+			<header class="container-fluid">
+			<div class="row">
+				<div class="container">
+					<div class="row">
+						<h1 class="col-sm-4">FishyFinds</h1>
+						<nav class="col-sm-8">
+							<a href="">Sign in</a>
+							<a href="">Register</a>
+							<a href="">menu 3</a>
+							<a href="">menu 4</a>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<div class="container-fluid main-image hidden-xs" >
+			<div class="row">
+				<div class="container"> 
+					<div class="row">
+						<div class="col-xs-12"> 
+							<p class="title"> FishyFinds </p>
+							<p class="slogan"> Plan your next trip with confidence</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-4 boxes">
+					<div class="box">
+						<img src="images/bungalows-image-ver1.jpg" class="img-rounded">
+						<h2>Bungalows</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat congue elementum. In auctor vulputate convallis. Nullam quis tempus urna, nec tempus enim. Nullam pulvinar, risus vitae iaculis dictum, mauris arcu pretium dolor, eget tincidunt sem augue vitae quam. Suspendisse egestas, magna dignissim mollis ultricies, ante elit pretium lectus, ut eleifend arcu erat vel neque. </p>	
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-4 boxes">
+					<div class="box">
+						<img src="images/boats-image-ver1.jpg">
+						<h2>Boats</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat congue elementum. In auctor vulputate convallis. Nullam quis tempus urna, nec tempus enim. Nullam pulvinar, risus vitae iaculis dictum, mauris arcu pretium dolor, eget tincidunt sem augue vitae quam. Suspendisse egestas, magna dignissim mollis ultricies, ante elit pretium lectus, ut eleifend arcu erat vel neque. </p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-4 boxes">
+					<div class="box">
+						<img src="images/courses-image-ver1.jpg">
+						<h2>Courses</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat congue elementum. In auctor vulputate convallis. Nullam quis tempus urna, nec tempus enim. Nullam pulvinar, risus vitae iaculis dictum, mauris arcu pretium dolor, eget tincidunt sem augue vitae quam. Suspendisse egestas, magna dignissim mollis ultricies, ante elit pretium lectus, ut eleifend arcu erat vel neque. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		`
+
+});

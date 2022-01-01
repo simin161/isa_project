d94c0f5b1bbe45ce275207d87e52c5b46c03a380
simin161 <73357823+simin161@users.noreply.github.com>
2022-01-01 22:07:07 +0100

    fix: code smell

--- src/main/java/com/fishyfinds/isa/mappers/DtoToDeleteRequest.java
+++ src/main/java/com/fishyfinds/isa/mappers/DtoToDeleteRequest.java
@@ -7,7 +7,6 @@ import java.util.Map;
 public class DtoToDeleteRequest {
 
     public static DeleteRequest MapToDeleteRequest(Map<String, String> map){
-        DeleteRequest d =  new DeleteRequest(Long.valueOf(map.get("id")), map.get("explanation"));
-        return d;
+        return new DeleteRequest(Long.valueOf(map.get("id")), map.get("explanation"));
     }
 }

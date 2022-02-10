bd5e74404c637f8cce1d58c762802395b8c7841b
simin161 <73357823+simin161@users.noreply.github.com>
2022-02-10 23:48:40 +0100

    feat: front for boats, bungalows - implemented sorting (#17)
    * feat: bungalow front page - unregistered user
    
    * feat: boats - unregistered user
    
    * fix: scripts
    
    * fix: boat/bungalows front page - unregistered user
    
    * feat: sorting bungalows (desc)
    
    * feat: sorting completed
    
    * feat: sorting - boats

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -7,16 +7,24 @@ INSERT INTO bungalow_owner values ('reason',1, 2, 1);
 
 INSERT INTO loyalty_program values(3, 20.5, 'baby shark', 0, 0);
 
---INSERT INTO location values (1, 'a',34.5, 23.4);
---INSERT INTO offer values(1, 'a', 'a', 'a', 3, 'Bungalow1', 1, 3.4, 'a', 100.5, 0, 1);
---INSERT INTO bungalow values(1);
+INSERT INTO location values (1, 'a', 'aa', 34.5, 23.4, 'aa', 4);
+INSERT INTO offer values(1, 'a', 'a', 'a', 3, 'Bungalow1', 1, 3.4, 'a', 100.5, 0, 1);
+INSERT INTO bungalow values(1);
 
---INSERT INTO engine values (1, 230.0, 4, 3500);
---INSERT INTO location values (2, 'location', 23.5, 44.5);
---INSERT INTO offer values(2, 'a', 'a', 'a', 3, 'Boat1', 1, 3.5, 'a', 100.5, 0, 2);
---INSERT INTO boat values(10, 'boat', '', '', 2,1);
+INSERT INTO engine values (1, 230.0, 4, 3500);
+INSERT INTO location values (2, 'location', 'loc', 23.5, 44.5, 'isda', 4);
+INSERT INTO offer values(2, 'a', 'a', 'a', 3, 'TBoat1', 1, 4.5, 'a', 120.5, 0, 2);
+INSERT INTO boat values(10, 'boat', '', '', 2,1);
 
---INSERT INTO engine values (2, 230.0, 4, 3500);
---INSERT INTO location values (3, 'location2', 23.5, 44.5);
---INSERT INTO offer values(3, 'a', 'a', 'a', 3, 'Boat2', 2, 3.5, 'a', 100.5, 0, 3);
---INSERT INTO boat values(10, 'boat', '', '', 3,2);
+INSERT INTO engine values (2, 230.0, 4, 3500);
+INSERT INTO location values (3, 'location2', 'loc2', 23.5, 44.5, 'ulica0', 3);
+INSERT INTO offer values(3, 'a', 'a', 'a', 3, 'Boat2', 2, 3.5, 'a', 100.5, 0, 3);
+INSERT INTO boat values(10, 'boat', '', '', 3,2);
+
+INSERT INTO public.location VALUES (4, 'Beograd', 'Ceca', 2, 1, 'Zabranjenog grada', 8);
+
+INSERT INTO public.offer(
+	id, additional_services, cancellation_policy, description, max_customer_capacity, offer_name, offer_type, rating, rules_of_conduct, unit_price, userid, location)
+	VALUES (4, 'blabla', 'blaaa', 'desc desc desc', 3, 'name offer', 0, 3.5, 'ponasanja pravila', 450, 1, 4);
+
+INSERT INTO public.bungalow values (4);

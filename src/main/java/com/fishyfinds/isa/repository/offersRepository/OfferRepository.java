492d414a71f5315d3c2a2bf04c20a864c6d6c5af
simin161 <shmepard@gmail.com>
2022-02-25 18:09:08 +0100

    fix: deleted unused method

--- src/main/java/com/fishyfinds/isa/repository/offersRepository/OfferRepository.java
+++ src/main/java/com/fishyfinds/isa/repository/offersRepository/OfferRepository.java
@@ -4,6 +4,4 @@ import com.fishyfinds.isa.model.beans.offers.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
-
-    public Offer findOneById(Long id);
 }

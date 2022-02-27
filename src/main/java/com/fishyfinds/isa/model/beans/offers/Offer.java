fdfbc4fcaeb6800bd199411e3b9dca7e3cf99394
simin161 <shmepard@gmail.com>
2022-02-27 11:12:17 +0100

    fix: user model and sql script

--- src/main/java/com/fishyfinds/isa/model/beans/offers/Offer.java
+++ src/main/java/com/fishyfinds/isa/model/beans/offers/Offer.java
@@ -3,6 +3,7 @@ package com.fishyfinds.isa.model.beans.offers;
 import com.fishyfinds.isa.model.beans.Term;
 import com.fishyfinds.isa.model.beans.UserFeedback;
 import com.fishyfinds.isa.model.beans.offers.bungalows.Bungalow;
+import com.fishyfinds.isa.model.beans.users.User;
 import com.fishyfinds.isa.model.enums.OfferType;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,8 +28,9 @@ public class Offer {
     @Column(name = "offerName", nullable = false)
     protected String offerName;
 
-    @Column(name = "userID")
-    protected int userID;
+    @OneToOne
+    @JoinColumn(name="users", referencedColumnName = "id")
+    protected User user;
 
     @OneToOne
     @JoinColumn(name="location", referencedColumnName = "id")

bb1ee4ce31c95188a54e94c05c2de0b0c8255ffa
simin161 <shmepard@gmail.com>
2022-05-30 23:27:15 +0200

    date parsing

--- src/main/java/com/fishyfinds/isa/service/termsService/TermsService.java
+++ src/main/java/com/fishyfinds/isa/service/termsService/TermsService.java
@@ -5,6 +5,8 @@ import com.fishyfinds.isa.model.beans.Term;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +28,8 @@ public class TermsService {
             //retVal = courseTermService.findAll();
             System.out.println("sdfa");
         //DateFormat: yyyy-mm-ddThh:mm
+        LocalDateTime startDateTime = LocalDateTime.parse(message.get("start"), DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime endDateTime = LocalDateTime.parse(message.get("end"), DateTimeFormatter.ISO_DATE_TIME);
         for(TermDto dto : retVal){
 
         }

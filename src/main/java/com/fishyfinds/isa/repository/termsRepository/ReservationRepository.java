81575f252941ddab1166aee9768d278c8e3086e6
simin161 <shmepard@gmail.com>
2022-08-21 19:46:38 +0200

    unreserved actions - database query

--- src/main/java/com/fishyfinds/isa/repository/termsRepository/ReservationRepository.java
+++ src/main/java/com/fishyfinds/isa/repository/termsRepository/ReservationRepository.java
@@ -2,7 +2,12 @@ package com.fishyfinds.isa.repository.termsRepository;
 
 import com.fishyfinds.isa.model.beans.terms.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Query( value = "SELECT * FROM reservation WHERE reservationType = 1 AND customer IS NULL", nativeQuery = true)
+    List<Reservation> findAllUnreservedActions();
 }

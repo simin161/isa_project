a643af8f8c12d72bfe8cb62d56e762d7687ac84d
simin161 <shmepard@gmail.com>
2022-09-01 00:25:24 +0200

    courses fronted refactored - showMore

--- src/main/resources/static/components/instructors.js
+++ src/main/resources/static/components/instructors.js
@@ -47,7 +47,7 @@ template: `
                 <br>
                 <br>
                 <div class="my-bungalows">
-                	<div class="col-md-4 left-div overflow-auto" style="margin-top:-20px; height:80vh">
+                	<div class="col-md-4 left-div overflow-auto" style="margin-top:-20px; margin-left: 22%; height:80vh" v-show="showPage == 0">
                 		<form class="justify-content-center">
                 			<table class="justify-content-center" style="width:90%; margin-left:5%; table-layout:fixed;" >
                 				<tr><td colspan="1"><input v-model="searchParams.courseName" class="update-text-profile" type="text" style="height:20px; font-size:12px; font-family:'poppins-light'" placeholder="Course's name" /></td>
@@ -84,75 +84,76 @@ template: `
                 					<div class="col-md-4" style="text-align:center;">
                 						<img src="../images/bungalow-images/register-instructor.png" class="img-fluid rounded" style="margin:0 auto;"alt="Instructor">
                 					</div>
-                					<div class="col-md-8">
-                						<div class="card-body">
-                                            <h5 class="card-title text-start mt-3" style="color:#fff;font-family:poppins-bold; font-size:15px;">{{course.offer.offerName}}</h5>
-                                            <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">Instructor: {{course.offer.user.firstName}} {{course.offer.user.lastName}}</p>
-                                            <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">{{course.offer.description}}</p>
-                                            <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">Unit price: {{course.offer.unitPrice}}</p>
-                                            <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">Rating: {{course.offer.rating}}</p>
-                                            <button class="float-end btn btn-light" @click="showMore(course.offer)" style="margin-left: 5px;">Show more</button>
-                                            <button class="float-end btn btn-light" @click="showTerms(course.offer)">Show terms</button>
-                                            <span v-show="loggedUser.userType === 'CUSTOMER'">
+                			    </div>
+                				<div class="col-md-8">
+                				    <div class="card-body">
+                                        <h5 class="card-title text-start mt-3" style="color:#fff;font-family:poppins-bold; font-size:15px;">{{course.offer.offerName}}</h5>
+                                        <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">Instructor: {{course.offer.user.firstName}} {{course.offer.user.lastName}}</p>
+                                        <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">{{course.offer.description}}</p>
+                                        <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">Unit price: {{course.offer.unitPrice}}</p>
+                                        <p class="card-text line-clamp-2" style="color:#fff;font-family:poppins-light; font-size:12px;">Rating: {{course.offer.rating}}</p>
+                                         <button class="float-end btn btn-light" @click="showMore(course.offer)" style="margin-left: 5px;">Show more</button>
+                                        <button class="float-end btn btn-light" @click="showTerms(course.offer)">Show terms</button>
+                                        <span v-show="loggedUser.userType === 'CUSTOMER'">
                                             <button v-show="!course.followed" class="float-end btn btn-light" style="background-color: #DED528; margin-left: 5px; margin-right: 5px;" @click="follow(course.offer)">Follow</button>
-                                            </span>
-                                        </div>
-                					</div>
+                                        </span>
+                                    </div>
                 				</div>
                 			</div>
-                		</div>
-                        </div>
-                          	<div class="col-md-4 right-div overflow-auto" style="margin-top:-20px; height:80vh" v-show="showPage == 1">
-                           	    <div class="container" v-show="showPage == 1">
-                		    	    <div class="container align-items-start">
-                						<input class="confirm-profile" type="button" value="Back" style="width:20%; float:left; font-size:12px; background-color: gray" @click="showPage = 0"/><br><br><br>
-                						<p class="title-text-bold" style="margin-top:10px; text-align:center;"> Show instructor </p>
-                						<form class="justify-content-center">
-                                            <table class="justify-content-center" style="width:75%; margin: auto; table-layout:fixed;" >
-                                            	<tr><td><input type="text" placeholder="   Course's name" class="input-text" v-model="courseToShow.offerName"/></td></tr><br>
-                                            	<tr class="d-flex justify-content-evenly">
-                                                    <td><input type="text" placeholder="   First Name" class="input-text"  v-model="courseToShow.user.firstName"/></td>
-                                                    <td><input type="text" placeholder="   Last Name" class="input-text"  v-model="courseToShow.user.lastName"/></td></tr><br>
-                                                <tr>
-                                                <tr class="d-flex justify-content-evenly">
-                                                    <td><input type="text" placeholder="   E-mail" class="input-text"  v-model="courseToShow.user.email"/></td>
-                                                    <td><input type="text" placeholder="   Phone number" class="input-text"  v-model="courseToShow.user.phoneNumber"/></td></tr><br>
-                                                <tr>
-                                        		<tr><textarea rowspan="3"name="text" placeholder="   Biography" class="input-text-area"  v-model="courseToShow.user.biography" ></textarea></tr><br>
-                                            	<tr class="d-flex justify-content-evenly">
-                                                	<td><input type="text" placeholder="   Country" class="input-text"  v-model="courseToShow.location.country"/></td>
-                                            	    <td><input type="text" placeholder="   City" class="input-text"  v-model="courseToShow.location.city"/></td></tr><br>
-                                            	<tr><td><input type="text" placeholder="   Street" class="input-text"  v-model="courseToShow.location.street"/></td></tr><br>
-                                            	<tr><td><input type="text" placeholder="   Street number" class="input-text"  v-model="courseToShow.location.streetNumber"/></td></tr><br>
-                                            	<tr><td><input type="text" placeholder="   Unit price" class="input-text"  v-model="courseToShow.unitPrice"/></td></tr><br>
-                                            	<tr><textarea rowspan="3" name="text" placeholder="   Description" class="input-text-area"  v-model="courseToShow.description" ></textarea></tr><br>
-                                            	<tr><td><input type="text" placeholder="   Maximum capacity" class="input-text"  v-model="courseToShow.maxCustomerCapacity"/></td></tr><br>
-                                        		<tr><textarea rowspan="3"name="text" placeholder="   Additional services (Wi-fi, Parking, etc.)" class="input-text-area"  v-model="courseToShow.additionalServices" ></textarea></tr><br>
-                                            	<tr><textarea rowspan="3" name="text" placeholder="   Rules of Conduct" class="input-text-area"  v-model="courseToShow.rulesOfConduct" ></textarea></tr><br>
-                                        		<tr><textarea rowspan="3" name="text" placeholder="   Cancellation policy" class="input-text-area"  v-model="courseToShow.cancellationPolicy" ></textarea></tr><br>
-                                        	</table>
-                                        </form>
-                					</div>
-                               	</div>
-                           	</div>
-                           	<div class="col-md-4 right-div overflow-auto" style="margin-top:-20px; height:80vh" v-show="showPage == 2">
-                                            <div class="container" v-show="showPage == 2">
-                                                        <div class="container align-items-start">
-                                                <input class="confirm-profile" type="button" value="Back" style="width:20%; float:left; font-size:12px; background-color: gray" @click="showPage = 0"/><br><br><br>
-                                                <p class="title-text-bold" style="margin-top:10px; text-align:center;">Show all terms</p>
-                                                <div v-for="term in terms" style="border-bottom: solid thick white">
-                                                    <p style="color:#fff;font-family:poppins-light; font-size:12px;">Start date: {{term.startTime}}</p>
-                                                    <p style="color:#fff;font-family:poppins-light; font-size:12px;">End date: {{term.endTime}}</p>
-                                                    <span v-show="loggedUser.userType === 'CUSTOMER'">
-                                                        <button class="float-end btn btn-light" @click="showReservation(term)">Make reservation</button>
-                                                    </span>
-                                                    <br/>
-                                                    </hr>
-                                                </div>
-                                            </div>
-                                       </div>
+                	    </div>
+                    </div>
+                    <div class="col-md-4 left-div overflow-auto" style="margin-top:-20px; margin-left: 22%; height:80vh" v-show="showPage == 1">
+                         <div class="container" v-show="showPage == 1">
+                		    <div class="container align-items-start">
+                				<input class="confirm-profile" type="button" value="Back" style="width:20%; float:left; font-size:12px; background-color: #881A02" @click="showPage = 0"/><br><br><br>
+                				<p class="title-text-bold" style="margin-top:10px; text-align:center;"> Show instructor </p>
+                				<form class="justify-content-center">
+                                     <table class="justify-content-center" style="width:75%; margin: auto; table-layout:fixed;" >
+                                         <tr><td><input type="text" placeholder="   Course's name" class="input-text" v-model="courseToShow.offerName"/></td></tr><br>
+                                         <tr class="d-flex justify-content-evenly">
+                                             <td><input type="text" placeholder="   First Name" class="input-text"  v-model="courseToShow.user.firstName"/></td>
+                                             <td><input type="text" placeholder="   Last Name" class="input-text"  v-model="courseToShow.user.lastName"/></td>
+                                         </tr><br>
+                                         <tr class="d-flex justify-content-evenly">
+                                              <td><input type="text" placeholder="   E-mail" class="input-text"  v-model="courseToShow.user.email"/></td>
+                                              <td><input type="text" placeholder="   Phone number" class="input-text"  v-model="courseToShow.user.phoneNumber"/></td>
+                                         </tr><br>
+                                        <tr><textarea rowspan="3"name="text" placeholder="   Biography" class="input-text-area"  v-model="courseToShow.user.biography" ></textarea></tr><br>
+                                        <tr class="d-flex justify-content-evenly">
+                                            <td><input type="text" placeholder="   Country" class="input-text"  v-model="courseToShow.location.country"/></td>
+                                            <td><input type="text" placeholder="   City" class="input-text"  v-model="courseToShow.location.city"/></td></tr><br>
+                                        <tr><td><input type="text" placeholder="   Street" class="input-text"  v-model="courseToShow.location.street"/></td></tr><br>
+                                        <tr><td><input type="text" placeholder="   Street number" class="input-text"  v-model="courseToShow.location.streetNumber"/></td></tr><br>
+                                        <tr><td><input type="text" placeholder="   Unit price" class="input-text"  v-model="courseToShow.unitPrice"/></td></tr><br>
+                                        <tr><textarea rowspan="3" name="text" placeholder="   Description" class="input-text-area"  v-model="courseToShow.description" ></textarea></tr><br>
+                                        <tr><td><input type="text" placeholder="   Maximum capacity" class="input-text"  v-model="courseToShow.maxCustomerCapacity"/></td></tr><br>
+                                        <tr><textarea rowspan="3"name="text" placeholder="   Additional services (Wi-fi, Parking, etc.)" class="input-text-area"  v-model="courseToShow.additionalServices" ></textarea></tr><br>
+                                        <tr><textarea rowspan="3" name="text" placeholder="   Rules of Conduct" class="input-text-area"  v-model="courseToShow.rulesOfConduct" ></textarea></tr><br>
+                                        <tr><textarea rowspan="3" name="text" placeholder="   Cancellation policy" class="input-text-area"  v-model="courseToShow.cancellationPolicy" ></textarea></tr><br>
+                                     </table>
+                                </form>
+                			</div>
+                       	</div>
+                    </div>
+                    <div class="col-md-4 left-div overflow-auto" style="margin-top:-20px; margin-left: 22%; height:80vh" v-show="showPage == 2">
+                        <div class="container" v-show="showPage == 2">
+                            <div class="container align-items-start">
+                                 <input class="confirm-profile" type="button" value="Back" style="width:20%; float:left; font-size:12px; background-color: #881A02" @click="showPage = 0"/><br><br><br>
+                                 <p class="title-text-bold" style="margin-top:10px; text-align:center;">Show all terms</p>
+                                 <div v-for="term in terms" style="border-bottom: solid thick white">
+                                      <p style="color:#fff;font-family:poppins-light; font-size:12px;">Start date: {{term.startTime}}</p>
+                                      <p style="color:#fff;font-family:poppins-light; font-size:12px;">End date: {{term.endTime}}</p>
+                                      <span v-show="loggedUser.userType === 'CUSTOMER'">
+                                             <button class="float-end btn btn-light" @click="showReservation(term)">Make reservation</button>
+                                      </span>
+                                      <br/>
+                                      </hr>
+                                 </div>
+                            </div>
                         </div>
                     </div>
+                </div>
+            </div>
           `
           ,
           computed: {
@@ -169,6 +170,9 @@ template: `
           }
           ,
           methods : {
+          showActions : function(id){
+                          router.push("actions/" + id);
+                      },
             showReservation : function(term){
                           router.push('/reservationForm/' + term.id);
             },
